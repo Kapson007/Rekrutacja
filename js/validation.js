0dfdb03afb70e7c09494ed49e320c481eee5a204
Kacper Nadstoga <kapix007@gmail.com>
2021-10-06 12:36:43 +0200

    add check inputs without passwords

--- js/validation.js
+++ js/validation.js
@@ -17,97 +17,61 @@ const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const password1 = document.getElementById('password1');
 const password2 = document.getElementById('password2');
-// const messageContainer = document.querySelector('.message-container');
-// const message = document.getElementById('message');
-const inputs = [firstName, lastName, email, password1, password2];
+const inputs = [firstName, lastName, email];
+// flag to check all inputs 
+let isFilled = false;
+let passwordsMatch = false;
 
-let isValid = false;
-let passwordMatch = false;
-
-function validation(e){
-    // let errorsContainer = [];
-    
-
-    // inputs.forEach(el => el.addEventListener('input', e => errorTag(e.target, !e.target.checkValidity())));
-    
-    // console.log('hello from validate function!');
-
-    // // name
-    // if(!name.checkValidity()){
-    //     errorsContainer.push('Pole z imieniem jest nieprawidłowo wypełnione');
-        
-    // }
-    // // last name
-    // if(!lastName.checkValidity()){
-    //     errorsContainer.push('Pole z imieniem jest nieprawidłowo wypełnione');
-    // }
-    // if(!email.checkValidity()){
-    //     errorsContainer.push("Pole z adresem email jest nieprawidłowo wypełnione");
-    // }
-    
-    // return true;
-
-    isValid = form.checkValidity();
-
-    if(!isValid){
-        const messageContainer = document.createElement('small');
-        messageContainer.textContent = 'Please fill out ald fields';
-        messageContainer.classList.add('error');
-        messageContainer.style.borderColor = 'red';
-        firstName.after(messageContainer);
-        return;
-    }
-    if(password1.value === password2.value){
-        passwordMatch = true;
-        password1.style.borderColor = 'green';
-        password2.style.borderColor = 'green';
-
-    }else{
-        passwordMatch = false;
-        const messageContainer = document.createElement('small');
-        messageContainer.textContent = 'Make sure passwords match';
-        messageContainer.classList.add('error');
-        messageContainer.style.borderColor = 'red';
-        return;
-    }
-
-    if(passwordMatch && isValid){
-        const messageContainer = document.createElement('small');
-        messageContainer.textContent = 'successfully registered';
-        messageContainer.classList.add('success');
-    }
-
-
-}
-
-function storeFormData(){
-    const data = {
-        firstName: form.firstName.value,
-        lastName: form.lastName.value,
-        email: form.email.value,
-        password: form.password.value
-    }
-    console.log(data);
+function processValidity(){
+    checkValidity();
+    checkPasswords();
 }
 
 
 
-
-
-
-function proccessForm(event){
-    event.preventDefault();
-
-
-    validation();
-    if(password1.value === password2.value){
-        storeFormData();
-    }
-
-    console.log('hello from processForm');
+function checkValidity(){
+    inputs.forEach(toCheck => {
+        if(toCheck.value === ''){
+            setErrorMessage(toCheck);
+        }else if(!toCheck.checkValidity()){
+            setErrorMessage(toCheck, toCheck.dataset.error);
+        }else{
+            setSuccess(toCheck);
+        }
+    });
+    isFilled = form.checkValidity();
 }
 
-// Run validation after DOM load on page
-window.addEventListener('DOMContentLoaded', ()=>{
-    form.addEventListener('submit', proccessForm);
+function setErrorMessage(input, info="To pole nie moze być puste!"){
+    const formGroup = input.parentElement;
+    const message = formGroup.querySelector('small');
+    console.log(input.dataset.error);
+    message.textContent = `${info}`;
+    if(formGroup.classList.contains('success')){
+        formGroup.classList.remove('success');
+    }
+
+    formGroup.classList.add('error');
+}
+
+function checkPasswords(){
+    
+}
+
+function setSuccess(input){
+    const formGroup = input.parentElement;
+    const message = formGroup.querySelector('small');
+    if(formGroup.classList.contains('error')){
+        formGroup.classList.remove('error');
+    }
+    formGroup.classList.add('success');
+}
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    processValidity();
+
 });
+
+
